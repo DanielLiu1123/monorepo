11d0cd60774a1afea042bb1bf9fe9435455b3ecb
Freeman Liu <llw599502537@gmail.com>
2025-11-18 00:57:38 +0800

    common

---
+++ packages/lib-java/common/src/main/java/monorepo/lib/common/MonoAutoConfiguration.java
@@ -0,0 +1,14 @@
+package monorepo.lib.common;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ *
+ *
+ * @author Freeman
+ * @since 2025/11/18
+ */
+@AutoConfiguration
+public class MonoAutoConfiguration {
+
+}
