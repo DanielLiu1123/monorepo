aa4104960c25ff6790182474b85eb951b3da1c55
Freeman Liu <llw599502537@gmail.com>
2025-11-20 23:51:15 +0800

    add opentelemetryHeaders

--- packages/lib-java/common/src/main/java/monorepo/lib/common/context/ContextConsts.java
+++ packages/lib-java/common/src/main/java/monorepo/lib/common/context/ContextConsts.java
@@ -1,6 +1,8 @@
 package monorepo.lib.common.context;
 
+import java.util.List;
 import java.util.Set;
+import java.util.function.BiPredicate;
 import java.util.regex.Pattern;
 
 /**
@@ -12,5 +14,17 @@ import java.util.regex.Pattern;
 public final class ContextConsts {
     private ContextConsts() {}
 
-    public static final Set<Pattern> propagatedHeaders = Set.of();
+    /**
+     * @see <a href="https://www.w3.org/TR/2021/REC-trace-context-1-20211123/#design-overview">Trace Context</a>
+     */
+    public static final Set<String> opentelemetryHeaders = Set.of(
+            "traceparent",
+            "tracestate"
+    );
+
+    public static final Set<BiPredicate<String, List<String>>> propagatedHeaders = Set.of(
+            (key, _) -> "Authorization".equalsIgnoreCase(key),
+            // opentelemetry traces headers
+            (key, _) -> opentelemetryHeaders.contains(key)
+    );
 }
