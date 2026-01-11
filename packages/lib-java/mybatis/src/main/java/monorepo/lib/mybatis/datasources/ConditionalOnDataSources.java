7a5cc98c60402409512ca9be6204c68d7424bbc9
Freeman Liu <llw599502537@gmail.com>
2026-01-11 10:10:02 +0800

    Refactor `DataSourcesConfiguration`: extract `MultipleDataSourcesCondition` into reusable `@ConditionalOnDataSources` annotation.

---
+++ packages/lib-java/mybatis/src/main/java/monorepo/lib/mybatis/datasources/ConditionalOnDataSources.java
@@ -0,0 +1,31 @@
+package monorepo.lib.mybatis.datasources;
+
+import java.lang.annotation.*;
+import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * Conditional on the presence of multiple data sources.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Conditional(MultipleDataSourcesCondition.class)
+public @interface ConditionalOnDataSources {}
+
+class MultipleDataSourcesCondition extends SpringBootCondition {
+    @Override
+    public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        var properties = Binder.get(context.getEnvironment())
+                .bindOrCreate(DataSourcesProperties.PREFIX, DataSourcesProperties.class);
+        var configured = !properties.datasources().isEmpty();
+        if (configured) {
+            return ConditionOutcome.match();
+        } else {
+            return ConditionOutcome.noMatch("No multiple data sources configured.");
+        }
+    }
+}
