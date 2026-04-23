570ea3f63584d6344123761131d5d00c26939867
Freeman Liu <llw599502537@gmail.com>
2026-04-24 02:33:10 +0800

    rm unused code

--- services/todo-service/src/main/java/monorepo/services/todo/entity/typehandler/TodoStateTypeHandler.java
+++
@@ -1,6 +0,0 @@
-package monorepo.services.todo.entity.typehandler;
-
-import monorepo.lib.mybatis.typehandler.BaseProtobufEnumNumberTypeHandler;
-import monorepo.proto.todo.v1.Todo;
-
-public class TodoStateTypeHandler extends BaseProtobufEnumNumberTypeHandler<Todo.State> {}
